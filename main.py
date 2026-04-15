d31ce4e9aec47c425e8d6150f34769aa537bc5f0
Mazhar31 <mzrbutt31@gmail.com>
2026-04-15 22:21:58 +0500

    updated

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ app = FastAPI(title="TAIGA Compliance API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
